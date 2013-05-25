ba29b6ecf27afae10273758380ce97710ead1b77
leo <facenord.sud@gmail.com>
2013-05-25 11:45:05 +0200

    petitie modif

--- category.php
+++ category.php
@@ -8,7 +8,6 @@ get_header();?>
         <?php if (have_posts()) : while (have_posts()) : the_post();?> 
             <li>
                 <h3><a href="<?php the_permalink(); ?>"><?php
-                echo get_the_excerpt().'<br/>';
                 the_title(); ?></a></h3>
                 <article><?php the_content('(lire la suite)'); ?></article>
             </li>
